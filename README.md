0d1874342e808ea722ddad968b96dd36946dac2c
Hemephelus <nwachukwuujubuonu@gmail.com>
2023-07-13 15:31:13 +0100

    created test function

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Code_Den_Code_Wars
-Solutions to code wars training
+This is a weekly meet up where developers try to solve code wars problem.
+
+We encourage interesting Ideas, different solutions to problems.
+
+You can add to the solutions we via a pull request or you can join us every Monday 6pm WAT to participate in out live class.
--- test.js
+++ test.js
@@ -1,22 +1,8 @@
-function solution1(integer) {
-  let arr = [];
-  for (let i = 2; i < integer; i++) {
-    if (integer % i === 0) {
-      arr.push(i);
-    }
-  }
-  return arr.length === 0 ? `${integer} is prime` : arr;
-}
-
 // SEE RESULTS
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-let inputs = [
-    { input: 12, expected_result: [2, 3, 4, 6] },
-    { input: 15, expected_result: [3, 5] },
-    { input: 13, expected_result: "13 is prime" },
-];
+let inputs = [{ input: "", expected_result: "" }];
 
-let tester = test([solution1], 1, inputs, 3);
+let tester = test([], 1, inputs, 3);
 console.log(tester);
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -30,17 +16,17 @@ console.log(tester);
  * @param {number} inputsIndex - Index of the input we want to test.
  */
 function test(funcs, funcIndex = 1, inputs, inputsIndex = 1) {
-    inputsIndex--;
-    const results = {};
-  
-    for (let i = 0; i < funcs.length; i++) {
-      results[i + 1] = {
-        actual_result: funcs[i](inputs[inputsIndex].input),
-        expected_result: inputs[inputsIndex].expected_result,
-      };
-    }
-  
-    return {
-      ...results[funcIndex],
+  inputsIndex--;
+  const results = {};
+
+  for (let i = 0; i < funcs.length; i++) {
+    results[i + 1] = {
+      actual_result: funcs[i](inputs[inputsIndex].input),
+      expected_result: inputs[inputsIndex].expected_result,
     };
   }
+
+  return {
+    ...results[funcIndex],
+  };
+}

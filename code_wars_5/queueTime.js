4f74afbfd749784c8e610159adf378de0ab2ae73
Hemephelus <nwachukwuujubuonu@gmail.com>
2023-08-07 21:46:55 +0100

    code wars 5 solutions

--- code_wars_5/queueTime.js
+++ code_wars_5/queueTime.js
@@ -4,50 +4,59 @@
 // Input
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 let inputs = [
-    { input: [[], 1], expected_result: 0 },
-    { input: [[1,2,3,4], 1], expected_result: 10 },
-    { input: [[2,2,3,3,4,4], 2], expected_result: 9 },
-    { input: [[1,2,3,4,5], 100], expected_result: 5  },
-    { input: [[5,3,4],    1], expected_result: 12  },
-    { input: [[10,2,3,3], 2], expected_result: 10  },
-    { input: [[2,3,10,2], 2], expected_result: 12  },
-  ];
-  
-  
-  // Functions
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  function solution1(customers, n){
-    
+  { input: [[], 1], expected_result: 0 },
+  { input: [[1, 2, 3, 4], 1], expected_result: 10 },
+  { input: [[2, 2, 3, 3, 4, 4], 2], expected_result: 9 },
+  { input: [[1, 2, 3, 4, 5], 100], expected_result: 5 },
+  { input: [[5, 3, 4], 1], expected_result: 12 },
+  { input: [[10, 2, 3, 3], 2], expected_result: 10 },
+  { input: [[2, 3, 10, 2], 2], expected_result: 12 },
+];
+
+// Functions
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function solution1(customers, n) {
+  // if (customers.length === 0) return 0;
+  // if (n === 1) return customers.reduce((prev, curr) => prev + curr);
+  // if (n > customers.length) return Math.max(...customers);
+
+  // console.log(customers, n);
+  let arr = Array(n).fill(0);
+
+  for (let i = 0; i < customers.length; i++) {
+    let index = arr.indexOf(Math.min(...arr))
+    arr[index] += customers[i]
   }
-  
-  
-  // test result
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  let tester = test([solution1], 1, inputs, 1);
-  console.log(tester.actual_result);
-  console.log(tester.expected_result);
-  
-  /**
-   * Test Multiple solutions and input at the same time.
-   * @constructor
-   * @param {Array} funcs - An array of functions solutions
-   * @param {number} funcIndex - Index of the function we want to test.
-   * @param {Array} inputs - An array of inputs to test the function
-   * @param {number} inputsIndex - Index of the input we want to test.
-   */
-  function test(funcs, funcIndex = 1, inputs, inputsIndex = 1) {
-    inputsIndex--;
-    const results = {};
-  
-    for (let i = 0; i < funcs.length; i++) {
-      results[i + 1] = {
-        actual_result: funcs[i](...inputs[inputsIndex].input),
-        expected_result: inputs[inputsIndex].expected_result,
-      };
-    }
-  
-    return {
-      ...results[funcIndex],
+
+  return Math.max(...arr)
+}
+
+// test result
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+let tester = test([solution1], 1, inputs, 7);
+console.log(tester.actual_result);
+console.log(tester.expected_result);
+
+/**
+ * Test Multiple solutions and input at the same time.
+ * @constructor
+ * @param {Array} funcs - An array of functions solutions
+ * @param {number} funcIndex - Index of the function we want to test.
+ * @param {Array} inputs - An array of inputs to test the function
+ * @param {number} inputsIndex - Index of the input we want to test.
+ */
+function test(funcs, funcIndex = 1, inputs, inputsIndex = 1) {
+  inputsIndex--;
+  const results = {};
+
+  for (let i = 0; i < funcs.length; i++) {
+    results[i + 1] = {
+      actual_result: funcs[i](...inputs[inputsIndex].input),
+      expected_result: inputs[inputsIndex].expected_result,
     };
   }
-  
+
+  return {
+    ...results[funcIndex],
+  };
+}
--- code_wars_5/unionJack.js
+++ code_wars_5/unionJack.js
@@ -1,51 +1,114 @@
 // URL
-// https://www.codewars.com/kata/5620281f0eeee479cd000020/train/javascript 
+// https://www.codewars.com/kata/5620281f0eeee479cd000020/train/javascript
 
 // Input
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 let inputs = [
-    { input: [7], expected_result: 'X--X--X\n-X-X-X-\n--XXX--\nXXXXXXX\n--XXX--\n-X-X-X-\nX--X--X' },
-    { input: [1], expected_result: 'X--X--X\n-X-X-X-\n--XXX--\nXXXXXXX\n--XXX--\n-X-X-X-\nX--X--X' },
-    { input: [8], expected_result: 'X--XX--X\n-X-XX-X-\n--XXXX--\nXXXXXXXX\nXXXXXXXX\n--XXXX--\n-X-XX-X-\nX--XX--X' },
-    { input: ['string'], expected_result: false },
-    { input: [10], expected_result: 'X---XX---X\n-X--XX--X-\n--X-XX-X--\n---XXXX---\nXXXXXXXXXX\nXXXXXXXXXX\n---XXXX---\n--X-XX-X--\n    -X--XX--X-\nX---XX---X' },
-  ];
-  
-  
-  // Functions
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  function solution1(x){
-    
+  {
+    input: [7],
+    expected_result:
+      "X--X--X\n-X-X-X-\n--XXX--\nXXXXXXX\n--XXX--\n-X-X-X-\nX--X--X",
+  },
+  {
+    input: [1],
+    expected_result:
+      "X--X--X\n-X-X-X-\n--XXX--\nXXXXXXX\n--XXX--\n-X-X-X-\nX--X--X",
+  },
+  {
+    input: [8],
+    expected_result:
+      "X--XX--X\n-X-XX-X-\n--XXXX--\nXXXXXXXX\nXXXXXXXX\n--XXXX--\n-X-XX-X-\nX--XX--X",
+  },
+  { input: ["string"], expected_result: false },
+  {
+    input: [10],
+    expected_result:
+      "X---XX---X\n-X--XX--X-\n--X-XX-X--\n---XXXX---\nXXXXXXXXXX\nXXXXXXXXXX\n---XXXX---\n--X-XX-X--\n-X--XX--X-\nX---XX---X",
+  },
+];
+
+// Functions
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+function solution1(x) {
+  if (isNaN(x)) return false;
+  console.log(x);
+  if (x < 7) {
+    x = 7;
   }
-  
-  
-  // test result
-  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  let tester = test([solution1], 1, inputs, 1);
-  console.log(tester.actual_result);
-  console.log(tester.expected_result);
-  
-  /**
-   * Test Multiple solutions and input at the same time.
-   * @constructor
-   * @param {Array} funcs - An array of functions solutions
-   * @param {number} funcIndex - Index of the function we want to test.
-   * @param {Array} inputs - An array of inputs to test the function
-   * @param {number} inputsIndex - Index of the input we want to test.
-   */
-  function test(funcs, funcIndex = 1, inputs, inputsIndex = 1) {
-    inputsIndex--;
-    const results = {};
-  
-    for (let i = 0; i < funcs.length; i++) {
-      results[i + 1] = {
-        actual_result: funcs[i](...inputs[inputsIndex].input),
-        expected_result: inputs[inputsIndex].expected_result,
-      };
+
+  x = Math.ceil(x)
+////////////////////////////////////////////
+  let oddNum = 7;
+  let evenNum = 8;
+  let seed = 2;
+  let arr = [];
+  let diff = 0;
+
+
+  if (x % 2 === 1) {
+    diff = (x - oddNum) / 2;
+  } else {
+    diff = (x - evenNum) / 2;
+  }
+
+  seed = seed ** (2 + diff);
+
+  for (let i = 0; i < x / 2 - 1; i++) {
+    if (x % 2 === 1) {
+      let newSeed =
+        "-".repeat(i) + seed.toString(2).replaceAll(1, "X").replaceAll(0, "-");
+
+      newSeed = newSeed + "X" + newSeed.split("").reverse().join("");
+      arr.push(newSeed);
+
+      seed = seed / 2;
+      console.log(seed.toString(2));
+    } else {
+      let newSeed =
+        "-".repeat(i) + seed.toString(2).replaceAll(1, "X").replaceAll(0, "-");
+
+      newSeed = newSeed + "XX" + newSeed.split("").reverse().join("");
+      arr.push(newSeed);
+
+      seed = seed / 2;
     }
-  
-    return {
-      ...results[funcIndex],
+  }
+
+  let cross =
+    x % 2 === 1 ? Array(1).fill("X".repeat(x)) : Array(2).fill("X".repeat(x));
+  let otherHalf = arr.map((a) => a).reverse();
+
+  arr.push(...cross, ...otherHalf);
+
+  return arr.join("\n");
+}
+
+// test result
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+let tester = test([solution1], 1, inputs, 5);
+console.log(tester.actual_result);
+console.log(tester.expected_result);
+
+/**
+ * Test Multiple solutions and input at the same time.
+ * @constructor
+ * @param {Array} funcs - An array of functions solutions
+ * @param {number} funcIndex - Index of the function we want to test.
+ * @param {Array} inputs - An array of inputs to test the function
+ * @param {number} inputsIndex - Index of the input we want to test.
+ */
+function test(funcs, funcIndex = 1, inputs, inputsIndex = 1) {
+  inputsIndex--;
+  const results = {};
+
+  for (let i = 0; i < funcs.length; i++) {
+    results[i + 1] = {
+      actual_result: funcs[i](...inputs[inputsIndex].input),
+      expected_result: inputs[inputsIndex].expected_result,
     };
   }
-  
+
+  return {
+    ...results[funcIndex],
+  };
+}
